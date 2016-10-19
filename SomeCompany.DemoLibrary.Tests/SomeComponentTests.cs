c654b9bea3d61b9c6f018f437fb17dbc1077e500
Guy Harwood <gpharwood@gmail.com>
2016-10-19 17:12:59 +0100

    initial commit

---
+++ SomeCompany.DemoLibrary.Tests/SomeComponentTests.cs
@@ -0,0 +1,28 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace SomeCompany.DemoLibrary.Tests
+{
+    [TestFixture]
+    public class SomeComponentTests
+    {
+        [Test]
+        public void SumCorrect()
+        {
+            var yearProviderMock = Substitute.For<IProvideYear>();
+            var currentYear = 2016;
+            var x = 123;
+            var y = 456;
+
+            yearProviderMock.GetYear().Returns(currentYear);
+
+            var sut = new SomeComponent(yearProviderMock);
+
+            var expected = currentYear + x + y;
+
+            var actual = sut.DoSomething(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ SomeCompany.DemoLibrary/SomeComponent.cs
@@ -0,0 +1,24 @@
+﻿namespace SomeCompany.DemoLibrary
+{
+    public class SomeComponent
+    {
+        private readonly IProvideYear _yearProvider;
+
+        public SomeComponent(IProvideYear yearProvider)
+        {
+            _yearProvider = yearProvider;
+        }
+
+        public int DoSomething(int x, int y)
+        {
+            var result = x + y + _yearProvider.GetYear();
+
+            return result;
+        }
+    }
+
+    public interface IProvideYear
+    {
+        int GetYear();
+    }
+}
